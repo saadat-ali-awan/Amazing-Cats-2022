80bf5cd4a99e026310a038ee740de1086c7c3a4d
Saadat123456 <saadatlucky313@gmail.com>
2022-03-08 22:11:06 +0500

    Add Api Strings

---
+++ src/modules/strings.js
@@ -0,0 +1,8 @@
+export const customBinId = '41ad3b18a3e4';
+export const customBinBaseURL = 'https://json.extendsclass.com/bin/';
+export const customBinRequestDataURL = `${customBinBaseURL}${customBinId}/`;
+
+export const involvementApiId = 'DLDmubioaHmMXoEiTQjD';
+export const involvementApiBaseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+export const likesApiURL = `${involvementApiBaseURL}apps/${involvementApiId}/likes`;
+export const commentApiULR = `${involvementApiBaseURL}apps/${involvementApiId}/comments`;
