fa594e6428708ab3c78e638fabc08674c50d9f84
Saadat123456 <saadatlucky313@gmail.com>
2022-03-10 19:15:13 +0500

    Update Homepage Styles

--- src/modules/strings.js
+++ src/modules/strings.js
@@ -1,7 +1,6 @@
 export const customBinId = '935e9eb10381';
 export const customBinBaseURL = 'https://json.extendsclass.com/bin/';
 export const customBinRequestDataURL = `${customBinBaseURL}${customBinId}`;
-
 export const involvementApiId = 'DLDmubioaHmMXoEiTQjD';
 export const involvementApiBaseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 export const likesApiURL = `${involvementApiBaseURL}apps/${involvementApiId}/likes`;
