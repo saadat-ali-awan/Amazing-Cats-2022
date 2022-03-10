952b6bc26e329bed9b8763fc785e53dac482df3e
Saadat123456 <saadatlucky313@gmail.com>
2022-03-10 19:41:07 +0500

    Update Involement API ID

--- src/modules/strings.js
+++ src/modules/strings.js
@@ -2,7 +2,7 @@ export const customBinId = '935e9eb10381';
 export const customBinBaseURL = 'https://json.extendsclass.com/bin/';
 export const customBinRequestDataURL = `${customBinBaseURL}${customBinId}`;
 
-export const involvementApiId = 'DLDmubioaHmMXoEiTQjD';
+export const involvementApiId = '90OCSQzkw8DWo0wOR3li';
 export const involvementApiBaseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 export const likesApiURL = `${involvementApiBaseURL}apps/${involvementApiId}/likes`;
 export const commentApiULR = `${involvementApiBaseURL}apps/${involvementApiId}/comments`;
